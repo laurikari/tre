d0426f81e35aa438c3751ad72fd40f914c7317a4
Ferdinand Prantl <prantlf@gmail.com>
2013-05-25 17:52:03 +0200

    Warnings produced by the MS Visual Studion 2012 C compiler suppressed.
    Changing macros with "while(0)" to "while(0,0)" and "while(1)"
    to "while(1,1)". Apparently the MS developer like the owl-y eyes :-)
    Explicit casts to (tre_cint_t) (tre_char_t) at places where the conversion
    is obviously wanted.

--- lib/tre-match-utils.h
+++ lib/tre-match-utils.h
@@ -76,7 +76,7 @@
 	str_user_end = str_source->get_next_char(&next_c, &pos_add_next,      \
                                                  str_source->context);	      \
       }									      \
-  } while(/*CONSTCOND*/0)
+  } while(/*CONSTCOND*/(void)0,0)
 
 #else /* !TRE_MULTIBYTE */
 
@@ -107,7 +107,7 @@
 	str_user_end = str_source->get_next_char(&next_c, &pos_add_next,      \
                                                  str_source->context);	      \
       }									      \
-  } while(/*CONSTCOND*/0)
+  } while(/*CONSTCOND*/(void)0,0)
 
 #endif /* !TRE_MULTIBYTE */
 
@@ -132,7 +132,7 @@
 	str_user_end = str_source->get_next_char(&next_c, &pos_add_next,      \
 						 str_source->context);	      \
       }									      \
-  } while(/*CONSTCOND*/0)
+  } while(/*CONSTCOND*/(void)0,0)
 
 #endif /* !TRE_WCHAR */
 
