f7bd6dee3095a3454662be61ec2763bcae1c69e4
Dag-Erling Smørgrav <des@des.dev>
2024-07-26 00:33:16 +0200

    Work around a bug in Windows.
    On Windows, `iswprint(L'\t')` (but not `isprint(L'\t')`) improperly
    returns true.  Work around this by explicitly checking for L'\t' before
    calling `iswprint()`.
    
    This fixes #64.
    
    Inspired by the R repository.

--- lib/tre-parse.c
+++ lib/tre-parse.c
@@ -193,7 +193,15 @@ int tre_iscntrl_func(tre_cint_t c) { return tre_iscntrl(c); }
 int tre_isdigit_func(tre_cint_t c) { return tre_isdigit(c); }
 int tre_isgraph_func(tre_cint_t c) { return tre_isgraph(c); }
 int tre_islower_func(tre_cint_t c) { return tre_islower(c); }
-int tre_isprint_func(tre_cint_t c) { return tre_isprint(c); }
+int tre_isprint_func(tre_cint_t c)
+{
+  return
+#if defined(WIN32) && TRE_WCHAR
+    /* On Windows, iswprint(L'\t') incorrectly returns true. */
+    c != L'\t' &&
+#endif
+    tre_isprint(c);
+}
 int tre_ispunct_func(tre_cint_t c) { return tre_ispunct(c); }
 int tre_isspace_func(tre_cint_t c) { return tre_isspace(c); }
 int tre_isupper_func(tre_cint_t c) { return tre_isupper(c); }
